ffeea5c7aaa7fbf8da59c3bdebfb933703ecfe78
longvbptit <longvb.ptit@gmail.com>
2022-03-18 19:57:54 +0700

    add some file

--- src/main/java/main/service/impl/CustomerRegisterServiceImpl.java
+++ src/main/java/main/service/impl/CustomerRegisterServiceImpl.java
@@ -1,4 +1,14 @@
 package main.service.impl;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.transaction.Transactional;
+
+@Service
+@Repository
+@Transactional(rollbackOn = Exception.class)
+@EnableTransactionManagement(proxyTargetClass = true)
 public class CustomerRegisterServiceImpl {
 }
